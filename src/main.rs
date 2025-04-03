36e113d18657780888a9ecc0805db20e096a7335
Carsten Ramke <carsten.ramke@gmail.com>
2025-04-03 08:05:10 +0200

    Add some button and text fields for fov

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use std::error::Error;
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
+    let ui = CameraCalculatorUI::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_calculate({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            ui.set_focal_length(ui.get_fov() + 1 as f32);
         }
     });
 
