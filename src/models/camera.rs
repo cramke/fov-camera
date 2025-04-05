07d4dc0789171bcff56cb402ac21145c964de025
Carsten Ramke <carsten.ramke@gmail.com>
2025-04-05 09:03:48 +0200

    Add missing fields to camera struct

--- src/models/camera.rs
+++ src/models/camera.rs
@@ -1,30 +1,41 @@
+
 pub struct Camera {
-    width: u32,
-    height: u32,
+    width_mm: f32,
+    height_mm: f32,
     width_pixels: u32,
     height_pixels: u32,
-    pixel_aspect_ratio: f32,
+    aspect_ratio: f32,
+    focal_length: f32,
+    fov: f32,
+    working_distance: f32,
+    ppm: f32,
+    gsd: f32,
     format: String,
 }
 
 impl Camera {
     pub fn new() -> Self {
         Camera {
-            width: 0,
-            height: 0,
+            width_mm: 0.0,
+            height_mm: 0.0,
             width_pixels: 0,
             height_pixels: 0,
-            pixel_aspect_ratio: 1.0,
+            aspect_ratio: 1.0,
+            focal_length: 0.0,
+            fov: 0.0,
+            working_distance: 0.0,
+            ppm: 0.0,
+            gsd: 0.0,
             format: String::new(),
         }
     }
 
     // Setters
-    pub fn set_width(&mut self, width: u32) {
-        self.width = width;
+    pub fn set_width(&mut self, width: f32) {
+        self.width_mm = width;
     }
-    pub fn set_height(&mut self, height: u32) {
-        self.height = height;
+    pub fn set_height(&mut self, height: f32) {
+        self.height_mm = height;
     }
     pub fn set_width_pixels(&mut self, width_pixels: u32) {
         self.width_pixels = width_pixels;
@@ -32,18 +43,34 @@ impl Camera {
     pub fn set_height_pixels(&mut self, height_pixels: u32) {
         self.height_pixels = height_pixels;
     }
-    pub fn set_pixel_aspect_ratio(&mut self, pixel_aspect_ratio: f32) {
-        self.pixel_aspect_ratio = pixel_aspect_ratio;
+    pub fn set_aspect_ratio(&mut self, aspect_ratio: f32) {
+        self.aspect_ratio = aspect_ratio;
+    }
+    pub fn set_focal_length(&mut self, focal_length: f32) {
+        self.focal_length = focal_length;
+    }
+    pub fn set_fov(&mut self, fov: f32) {
+        self.fov = fov;
+    }
+    pub fn set_working_distance(&mut self, working_distance: f32) {
+        self.working_distance = working_distance;
+    }
+    pub fn set_ppm(&mut self, ppm: f32) {
+        self.ppm = ppm;
+    }
+    pub fn set_gsd(&mut self, gsd: f32) {
+        self.gsd = gsd;
     }
     pub fn set_format(&mut self, format: String) {
         self.format = format;
     }
+
     // Getters
-    pub fn get_width(&self) -> u32 {
-        self.width
+    pub fn get_width_mm(&self) -> f32 {
+        self.width_mm
     }
-    pub fn get_height(&self) -> u32 {
-        self.height
+    pub fn get_height_mm(&self) -> f32 {
+        self.height_mm
     }
     pub fn get_width_pixels(&self) -> u32 {
         self.width_pixels
@@ -51,8 +78,23 @@ impl Camera {
     pub fn get_height_pixels(&self) -> u32 {
         self.height_pixels
     }
-    pub fn get_pixel_aspect_ratio(&self) -> f32 {
-        self.pixel_aspect_ratio
+    pub fn get_aspect_ratio(&self) -> f32 {
+        self.aspect_ratio
+    }
+    pub fn get_focal_length(&self) -> f32 {
+        self.focal_length
+    }
+    pub fn get_fov(&self) -> f32 {
+        self.fov
+    }
+    pub fn get_working_distance(&self) -> f32 {
+        self.working_distance
+    }
+    pub fn get_ppm(&self) -> f32 {
+        self.ppm
+    }
+    pub fn get_gsd(&self) -> f32 {
+        self.gsd
     }
     pub fn get_format(&self) -> &str {
         &self.format
